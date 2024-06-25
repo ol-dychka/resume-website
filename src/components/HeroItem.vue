219961222f41a455c62785022d632913037fe165
ol-dychka <ol.dychka@gmail.com>
2024-06-24 20:06:22 -0400

    added resume

--- src/components/HeroItem.vue
+++ src/components/HeroItem.vue
@@ -4,16 +4,14 @@
   <div class="px-32 py-12" id="home">
     <div class="flex justify-between items-center gap-12">
       <div class="space-y-8">
-        <p class="font-bold text-7xl text-teal-800">
-          Front-End React Developer
-        </p>
+        <p class="font-bold text-7xl text-teal-800">Fullstack Developer</p>
         <p>
           Hi, I'm Oleksandr Dychka. Passionate developer and avid learner based
           in Toronto, Canada
         </p>
       </div>
 
-      <div class="flex gap-4">
+      <div class="flex gap-4 items-center">
         <a
           href="https://linkedin.com/in/ol-dychka"
           target="_blank"
@@ -27,6 +25,13 @@
           class="hover:text-teal-800 duration-200"
         >
           <v-icon name="bi-github" scale="2.5" />
+        </a>
+        <a
+          href="https://drive.google.com/file/d/14YHq2PgjxFsb3FAC_jEAGtne8RaS-Wos/view?usp=sharing"
+          target="_blank"
+          class="hover:text-teal-800 duration-200 text-2xl"
+        >
+          Resume
         </a>
       </div>
     </div>
