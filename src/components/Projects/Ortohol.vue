745d131effcc3f29ed20f11d7dfb0f0d40bd6f83
ol-dychka <ol.dychka@gmail.com>
2024-08-05 17:43:09 -0400

    updated description

--- src/components/Projects/Ortohol.vue
+++ src/components/Projects/Ortohol.vue
@@ -16,10 +16,10 @@ const getImageUrl = () => {
       <div>
         <p class="text-3xl">Orthopedics Ecommerce Website</p>
         <p class="font-normal mt-4">
-          This website was developed for an e-commerce business. Features:
-          Pagination, Search, Filtering, Login, Payment w/ Stripe. Please note
-          that up-to-day code on Github and published version might have a few
-          differences.
+          [Can take about a minute to start] This website was developed for an
+          e-commerce business. Features: Pagination, Search, Filtering, Login,
+          Payment w/ Stripe. Please note that up-to-day code on Github and
+          published version might have a few differences.
         </p>
         <p class="font-normal mt-4">
           Backend: C# with layered architecture, PostgreSQL, Docker. Frontend:
